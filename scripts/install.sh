0fecf81d96454a3ac9f66b9625c143f8fa381791
Stiven Roytman <stivenr@Stivens-MacBook-Pro.local>
2025-04-17 01:53:53 -0400

    Have to use global Python installation to initialize the environment.

--- scripts/install.sh
+++ scripts/install.sh
@@ -2,10 +2,11 @@
 
 ROOT=$(dirname $(dirname $0))
 cd $ROOT
+echo $(pwd)
 
 if [ ! -d env ]; then
 	python -m venv env
-	env/bin/pip install -r requirements.txt
+	$ROOT/env/bin/pip install -r requirements.txt
 else
 	echo 'Program is already installed!'
 fi
