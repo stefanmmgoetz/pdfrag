0f201873577e9e77f6cd323b6ce03849c1b26a0b
Stiven Roytman <stivenr@Stivens-MacBook-Pro.local>
2025-04-17 02:42:02 -0400

    Added interactive query

--- scripts/query.sh
+++ scripts/query.sh
@@ -4,6 +4,9 @@ ROOT=$(dirname $(dirname $0))
 cd $ROOT
 source env/bin/activate
 
-read -p 'Number of sentences: ' numsentences
-read -p 'Query: ' query
-python src/query_pdfs.py . $numsentences "$query" 2>/dev/null
+while true; do
+	read -p 'Number of sentences: ' numsentences
+	read -p 'Query: ' query
+	python src/query_pdfs.py . $numsentences "$query" 2>/dev/null
+done
+	
